488b900731028908663fa0d7b3ece123e0b00038
ekrap <ptrece8@gmail.com>
2024-08-22 23:44:12 +0000

    architecture's update

--- student/models.py
+++ student/models.py
@@ -3,12 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=100)
     register = models.CharField(max_length=30, unique=True)
     birth_date = models.DateField()
     classroom = models.CharField(max_length=20)
-    tel = models.PositiveIntegerField(unique=True, blank=True)
+    phone = models.PositiveIntegerField(unique=True, blank=True)
 
     def _str_(self) -> str:
-        return f"{self.register} - {self.name}"
+        return f"{self.register} - {self.name}" 
+    
